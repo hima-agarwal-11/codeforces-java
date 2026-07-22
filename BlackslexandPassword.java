6c5611211c06dc276ed1a171519e049346a5febe
Hima Agarwal <himaagarwal11@gmail.com>
2026-07-22 23:11:57 +0530

    Solve Codeforces A - Blackslex and Password

---
+++ BlackslexandPassword.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class BlackslexandPassword {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+  
+        while(t>0){
+          
+          int k = sc.nextInt();
+          int x=sc.nextInt();
+          System.out.println(k*x+1);
+          t--;
+        }
+  
+        sc.close();
+    }
+}
