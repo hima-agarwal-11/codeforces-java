33e3d42fb683c2534daea0c453920d62cb5daf69
Hima Agarwal <himaagarwal11@gmail.com>
2026-08-04 22:30:37 +0530

    Solve Codeforces 84A - Toy Army

---
+++ toyarmy.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    long n = sc.nextLong();
+    System.out.println((3*n)/2);
+    sc.close();
+
+}
