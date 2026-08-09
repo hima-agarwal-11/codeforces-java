f5e9db4fb97fa56a5dde03928f0fa7bc7a469072
Hima Agarwal <himaagarwal11@gmail.com>
2026-08-09 14:10:08 +0530

    Solve Codeforces 2118A - The Equalizer

---
+++ theequalizer.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class theequalizer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            long sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                sum += sc.nextInt();
+            }
+
+            if (sum % 2 == 1 || (1L * n * k) % 2 == 0)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+
+        sc.close();
+    }
+}
