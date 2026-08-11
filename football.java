4e18e5510bf131db3bd5b8bf96f61b7bba8b2dd4
Hima Agarwal <himaagarwal11@gmail.com>
2026-08-11 18:48:33 +0530

    Solve Codeforces 96A - Football

---
+++ football.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class football {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count >= 7) {
+                System.out.println("YES");
+                sc.close();
+                return;
+            }
+        }
+
+        System.out.println("NO");
+
+        sc.close();
+    }
+}
